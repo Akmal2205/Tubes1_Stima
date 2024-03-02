f9999776b2284ad7bbabb6fd12b8f4102c45ce85
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2024-03-03 03:59:26 +0700

    feat: enhance hunting diamond algorithm and avoid teleporter

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botgacor.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botgacor.py
@@ -6,23 +6,53 @@ from game.models import GameObject, Board, Position
 from ..util import get_direction
 
 def coordinate_diamond_ratio(coordinates, points, current_position):
-    sum_coordinates = [((point.x - current_position.x)**2 + (point.y-current_position.y)**2) for point in coordinates]
+    sum_coordinates = [((coordinate.x - current_position.x)**2 + (coordinate.y-current_position.y)**2) for coordinate in coordinates]
     ratio = []
 
     for i in range (len(sum_coordinates)):
-        if (sum_coordinates == 0):
+        if (sum_coordinates[i] == 0):
             ratio.append(-1)
         else:
             ratio.append(points[i]/sum_coordinates[i])
     
     return [coordinates, points], ratio
 
-def get_coordinate_goal_for_diamond(list_ratio, list_coordinates):
+def get_coordinate_goal_for_diamond(list_ratio, list_coordinates, inventory_diamonds, list_point):
+    
     # cari index dari nilai max list ratio
     max_ratio = max(list_ratio)
     index = list_ratio.index(max_ratio)
 
-    return list_coordinates[index]
+    while(list_point[index]==2 and inventory_diamonds==4):
+        list_ratio[index] = -1
+        max_ratio = max(list_ratio)
+        index = list_ratio.index(max_ratio)
+
+    if(list_ratio[index]==-1):
+        return list_coordinates[index], True
+
+    if(inventory_diamonds==4):
+        if(list_point[index]==2):
+            index-=1
+
+    return list_coordinates[index], False
+
+def avoid_teleport(goal_position, current_position, delta_x, delta_y):
+    # (0,0) itu kotak paling kiri atas
+    if(delta_x == 0): #pergerakan vertikal
+        # berarti bisa digeser kanan atau kiri, tergantung posisi relatif goal terhadap tujuan
+        selisih_x = goal_position.x - current_position.x # kalo positif artinya goal ada di sebelah kanan, kita geser ke kanan aja. kalaupun selisihnya nol, kita bebas mau ambil kanan atau kiri
+        if(selisih_x>=0):
+            return 1, 0
+        else:
+            return -1,0
+    else: # artinya delta_x bergerak ke kanan atau ke kiri, yang mana nilai delta_y pasti 0
+        # berarti bisa digeser atas atau bawah, tergantung posisi relatif goal terhadap tujuan
+        selisih_y = goal_position.y - current_position.y # kalo positif artinya goal ada di sebelah bawah, kita geser ke bawah aja. kalaupun selisihnya nol, kita bebas mau ambil atas atau bawah
+        if(selisih_y>=0):
+            return 0, 1
+        else:
+            return 0,-1
 
 class BotGacor(BaseLogic):
     def __init__(self):
@@ -31,41 +61,47 @@ class BotGacor(BaseLogic):
         self.current_direction = 0
 
     def next_move(self, board_bot: GameObject, board: Board):
+        # inisiasi
         props = board_bot.properties
         current_position = board_bot.position
-        print(current_position.x, current_position.y)
+        teleport_location = [object.position for object in board.game_objects if object.type == "TeleportGameObject"]
         coordinate_and_point, ratio = coordinate_diamond_ratio([coordinate.position for coordinate in board.diamonds], [coordinate.properties.points for coordinate in board.diamonds], current_position)
-        print(ratio)
-        print(coordinate_and_point)
-        goal = get_coordinate_goal_for_diamond(ratio, coordinate_and_point[0])
-        print(goal.x, goal.y)
+        goal, is_base = get_coordinate_goal_for_diamond(ratio, coordinate_and_point[0], props.diamonds, coordinate_and_point[1])
+        
+        print(teleport_location)
+        # print(current_position.x, current_position.y)
+        # print(ratio)
+        # print(coordinate_and_point)
+        # print(props.diamonds)
+        # print(goal.x, goal.y)
 
         # Analyze new state
-        if props.diamonds == 5:
+        base = board_bot.properties.base
+        if props.diamonds >= 5:
             # Move to base
-            base = board_bot.properties.base
             self.goal_position = base
         else:
             # Just roam around
-            self.goal_position = goal
+            if(not is_base):
+                self.goal_position = goal
+            else:
+                self.goal_position = base
 
-        if self.goal_position:
-            # We are aiming for a specific position, calculate delta
-            delta_x, delta_y = get_direction(
-                current_position.x,
-                current_position.y,
-                self.goal_position.x,
-                self.goal_position.y,
-            )
-        else:
-            # Roam around
-            delta = self.directions[self.current_direction]
-            delta_x = delta[0]
-            delta_y = delta[1]
-            if random.random() > 0.6:
-                self.current_direction = (self.current_direction + 1) % len(
-                    self.directions
-                )
-        
-        print (delta_x, delta_y)
+
+        # We are aiming for a specific position, calculate delta
+        delta_x, delta_y = get_direction(
+            current_position.x,
+            current_position.y,
+            self.goal_position.x,
+            self.goal_position.y,
+        )
+
+
+        # implementasi hindari teleport button
+        for tel in teleport_location:
+            if((delta_x + current_position.x) == tel.x and (delta_y + current_position.y) == tel.y):
+                delta_x, delta_y = avoid_teleport(self.goal_position, current_position, delta_x, delta_y)
+
+            
+
         return delta_x, delta_y

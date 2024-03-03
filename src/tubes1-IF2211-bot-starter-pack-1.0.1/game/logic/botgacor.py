e90de0defa92c18449bc2ce6d629953f6aa5cef3
Akmal2205 <syarafiakml2004@gmail.com>
2024-03-03 10:45:47 +0700

    ft : Branch Akmal

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botgacor.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/botgacor.py
@@ -24,6 +24,22 @@ def get_coordinate_goal_for_diamond(list_ratio, list_coordinates):
 
     return list_coordinates[index]
 
+def bot_scan(enemy_location, current_position):
+    dist = [((enemy.x - current_position.x)**2 + (enemy.y-current_position.y)**2) for enemy in enemy_location]
+    
+    for i in dist:
+        if i<=4 and i>0:
+            return True
+        else:
+            return False
+
+def att_bot(current_position, enemy_location):
+    if(bot_scan(enemy_location,current_position)):
+        if((enemy_location.x - current_position.x)**2 + (enemy_location.y-current_position.y)**2)==1:
+            return True#current location diarahkan ke enemy location
+        else:
+            return False#sleep, sekian detik menunggu
+            
 class BotGacor(BaseLogic):
     def __init__(self):
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
@@ -33,6 +49,7 @@ class BotGacor(BaseLogic):
     def next_move(self, board_bot: GameObject, board: Board):
         props = board_bot.properties
         current_position = board_bot.position
+        enemy_location = [d.position for d in board.bots if d.position.x!=0 and d.position.y!=0]
         print(current_position.x, current_position.y)
         coordinate_and_point, ratio = coordinate_diamond_ratio([coordinate.position for coordinate in board.diamonds], [coordinate.properties.points for coordinate in board.diamonds], current_position)
         print(ratio)
@@ -46,8 +63,10 @@ class BotGacor(BaseLogic):
             base = board_bot.properties.base
             self.goal_position = base
         else:
-            # Just roam around
-            self.goal_position = goal
+            if (att_bot(current_position, enemy_location)):#menyerang
+                self.goal_position = enemy_location
+            else:#defense mechanism (kabur)
+                self.goal_position = goal
 
         if self.goal_position:
             # We are aiming for a specific position, calculate delta
--- src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
@@ -1,2 +1,5 @@
 @echo off
-start cmd /c "python main.py --logic Gacor --email=test@email.com --name=stima --password=123456 --team etimo"
+start cmd /c "python main.py --logic Gacor --email=test1@email.com --name=stima1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Gacor --email=test2@email.com --name=stima2 --password=12345 --team etimo"
+start cmd /c "python main.py --logic Gacor --email=test3@email.com --name=stima3 --password=1234 --team etimo"
+start cmd /c "python main.py --logic Gacor --email=test4@email.com --name=stima4 --password=123 --team etimo"
